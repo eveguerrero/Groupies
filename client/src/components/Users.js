a61205a50c56b2576fca859995618406baa559dc
Eveline Guerrero <evelineguerrero23@gmail.com>
2022-05-05 12:16:24 -0400

    rendered users

---
+++ client/src/components/Users.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+
+function Users ({user}){
+
+  const id = Users.id
+      
+    return (
+        <li>
+            
+        <h1>{user.username}</h1>
+        
+        </li>
+    
+    )
+}
+
+export default Users;
